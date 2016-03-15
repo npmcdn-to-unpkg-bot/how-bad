1139729460e52fbca77684e8badc60bf94c7d76c
Emil Laine <emil.laine@cs.helsinki.fi>
2016-03-16 00:10:57 +0200

    Implement Review 'all', 'create', 'delete' methods

--- app/models/Review.scala
+++ app/models/Review.scala
@@ -1,13 +1,41 @@
 package models
 
+import play.api.db._
+import play.api.Play.current
+import anorm._
+import anorm.SqlParser._
+
 case class Review(id: Long, comments: String)
 
 object Review {
 
-  def all: List[Review] = Nil
+  val review = {
+    get[Long]("id") ~
+    get[String]("comments") map {
+      case id~comments => Review(id, comments)
+    }
+  }
 
-  def create(comments: String) {}
+  def all: List[Review] = {
+    DB.withConnection { implicit conn =>
+      SQL("select * from review").as(review *)
+    }
+  }
 
-  def delete(id: Long) {}
+  def create(comments: String) {
+    DB.withConnection { implicit conn =>
+      SQL("insert into review (comments) values ({comments})").on(
+        'comments -> comments
+      ).executeUpdate
+    }
+  }
+
+  def delete(id: Long) {
+    DB.withConnection { implicit conn =>
+      SQL("delete from review where id = {id}").on(
+        'id -> id
+      ).executeUpdate
+    }
+  }
 
 }
