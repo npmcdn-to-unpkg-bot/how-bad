40067f1f14fd3f16e444a9be1cce795b4c53f4c8
Emil Laine <emil.laine@cs.helsinki.fi>
2016-04-05 18:58:53 +0300

    Update Reviews database schema

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,6 +5,10 @@
 CREATE SEQUENCE review_id_seq;
 CREATE TABLE review (
   id integer NOT NULL DEFAULT nextval('review_id_seq'),
+  user_id integer NOT NULL,
+  movie text NOT NULL,
+  movie_id char(9) NOT NULL,
+  rating integer NOT NULL,
   comments text
 );
 
--- conf/evolutions/default/2.sql
+++
@@ -1,9 +0,0 @@
-# Update Review
-
-# --- !Ups
-
-ALTER TABLE review ADD movie text NOT NULL DEFAULT '?';
-
-# --- !Downs
-
-ALTER TABLE review DROP movie;
